597b6147d53d8fdee656f437382a6c73bde83f27
Daniel Mendler <mail@daniel-mendler.de>
2022-11-13 18:10:58 +0100

    Comment updates

--- scad-mode.el
+++ scad-mode.el
@@ -26,7 +26,8 @@
 ;;; Commentary:
 ;;
 ;; This is a major-mode to implement the SCAD constructs and
-;; font-locking for OpenSCAD. Install the package from MELPA:
+;; font-locking for OpenSCAD. Install the package from NonGNU ELPA or
+;; MELPA:
 ;;
 ;; M-x install-package RET scad-mode RET
 
