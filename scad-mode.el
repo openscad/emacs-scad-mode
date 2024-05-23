3c32b141f083c31539bb24700eb0aa23ea55918c
Daniel Mendler <mail@daniel-mendler.de>
2024-05-23 19:38:25 +0200

    Version 94.0

--- scad-mode.el
+++ scad-mode.el
@@ -6,7 +6,7 @@
 ;; Keywords: languages
 ;; Homepage: https://github.com/openscad/emacs-scad-mode
 ;; Package-Requires: ((emacs "27.1") (compat "29.1.4.4"))
-;; Version: 93.3
+;; Version: 94.0
 
 ;; This file is not part of GNU Emacs.
 
